8e3d7fa65d8b36346f83c3bf77fcffb7d2f9c049
mosh98 <mosleh.edu@gmail.com>
2023-02-22 15:23:41 +0100

    comments

--- src/main/java/com/example/demo/CustomerRepositoryImpl.java
+++ src/main/java/com/example/demo/CustomerRepositoryImpl.java
@@ -11,14 +11,6 @@ import java.util.List;
 
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
-    /**
-     * 4. Return a page of customers from the database. This should take in limit and offset as parameters and make use of the SQL limit and offset keywords to get a subset of the customer data.
-     *    The customer model from above should be reused.
-     *  5. Add a new customer to the database. You also need to add only the fields listed above (our customer object)
-     * 6. Update an existing customer. [DONE]
-     * 7. Return the country with the most customers.
-     * 8. Return the customer with the highest total.
-     */
 
     private final JdbcTemplate jdbcTemplate;
     private final String url;
@@ -38,6 +30,10 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public List<Customer> findAll() {
+        /**
+         * Return all customers from the database.
+         * @return List of all customers
+         */
         String sql = "SELECT * FROM customer";
         List<Customer> customers = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
@@ -69,6 +65,11 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public Customer findById(Integer id) {
+        /**
+         * Return a customer from the database with the given id.
+         * @param id The id of the customer to return
+         * @return The customer with the given id
+         */
         String sql = "SELECT * FROM customer WHERE customer_id = ?";
         Customer customer = null;
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
@@ -100,6 +101,13 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     //number 4
     @Override
     public List<Customer> getCustomers(int limit, int offset) {
+        /**
+         * Return a list of customers from the database with the given limit and offset.
+         * @param limit The number of customers to return
+         * @param offset The number of customers to skip
+         * @return A list of customers with the given limit and offset
+         */
+
         List<Customer> customers = new ArrayList<>();
 
         try (Connection connection = DriverManager.getConnection(url, username, password);
@@ -137,6 +145,12 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public Customer findByName(String name) {
+        /**
+         * Return a customer from the database with the given name.
+         * @param name The name of the customer to return
+         * @return The customer with the given name
+         * @throws RuntimeException if there is an error with the database
+         */
         String sql = "SELECT * FROM customer WHERE first_name LIKE ?";
         Customer customer = null;
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
@@ -168,6 +182,11 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     //number 5
     @Override
     public int addCustomer(Customer object) {
+        /**
+         * Add a customer to the database.
+         * @param object The customer to add
+         * @return The number of rows affected
+         */
         //Make an sql string to insert into the database for customer
         String sql = "INSERT INTO customer (first_name, last_name, company, address, city, state, country,  postal_code, phone,fax, email) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         return jdbcTemplate.update(sql, object.getFirst_name(),
@@ -186,6 +205,15 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     //number 6
     @Override
     public int updateCustomer(Customer object) {
+        /**
+         * Update a customer in the database.
+         * @param object The customer to update
+         * @return The number of rows affected
+         * @throws RuntimeException if there is an error with the database
+         * @throws IllegalArgumentException if the customer does not exist
+         * @throws IllegalArgumentException if the customer id is null
+         */
+
         String sql = "UPDATE customer SET first_name = ?, last_name = ?, company = ?, address = ?, city = ?, state = ?, country = ?,  postal_code = ?, phone = ?, fax = ?, email = ? WHERE customer_id = ?";
 
 
@@ -217,6 +245,11 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     //number 7
     @Override
     public String findCountryWithMostCustomers() {
+        /**
+         * Return the country with the most customers.
+         * @return The country with the most customers
+         * @throws RuntimeException if there is an error with the database
+         */
 
         String sql = "SELECT country, COUNT(*) AS count FROM customer GROUP BY country ORDER BY count DESC LIMIT 1";
 
@@ -242,6 +275,12 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     //number 8
     @Override
     public String findCustomerWithHighestTotal() {
+        /**
+         * Return the customer with the highest total spent.
+         * @return The customer with the highest total spent
+         * @throws RuntimeException if there is an error with the database
+         */
+
         String sql = "SELECT c.first_name, MAX(i.total) AS total_spent\n" +
                 "FROM customer c\n" +
                 "         JOIN invoice i ON c.customer_id = i.customer_id\n" +
