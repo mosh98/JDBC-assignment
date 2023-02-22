29af5bccf9467146855b619d5da43b84756b4d17
Zakhida Ruslanova <zakhida_sul@hotmail.se>
2023-02-22 08:00:18 +0100

    Fixed find all customers method, had to change customer class to customer record

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -1,42 +1,26 @@
 package com.example.demo;
 
-public class Customer {
-    private int id;
-    private String first_name;
+public record Customer(
+        int id,
+        String first_name,
 
-    private String last_name;
+        String last_name,
 
-    private String company;
+        String company,
 
-    private String address;
+        String address,
 
-    private String city;
+        String city,
 
-    private String state;
+        String state,
 
-    private String country;
+        String country,
 
-    private String postal_code;
+        String postal_code,
 
-    private String phone;
+        String phone,
 
-    private String fax;
+        String fax,
 
-    private String email;
-
-
-    public Customer(int id, String first_name, String last_name, String company, String address, String city, String state, String country, String postal_code, String phone, String fax, String email) {
-        this.id = id;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.company = company;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.country = country;
-        this.postal_code = postal_code;
-        this.phone = phone;
-        this.fax = fax;
-        this.email = email;
-    }
+        String email) {
 }
--- src/main/java/com/example/demo/CustomerRepositoryImpl.java
+++ src/main/java/com/example/demo/CustomerRepositoryImpl.java
@@ -1,16 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
     /**
      * 4. Return a page of customers from the database. This should take in limit and offset as parameters and make use of the SQL limit and offset keywords to get a subset of the customer data. The customer model from above should be reused.
-     *  5. Add a new customer to the database. You also need to add only the fields listed above (our customer object)
+     * 5. Add a new customer to the database. You also need to add only the fields listed above (our customer object)
      * 6. Update an existing customer.
      */
 
@@ -19,10 +22,11 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     private final String username;
     private final String password;
 
+    @Autowired
     public CustomerRepositoryImpl(
             JdbcTemplate jdbcTemplate, @Value("${spring.datasource.url}") String url,
             @Value("${spring.datasource.username}") String username,
-            @Value("${spring.datasource.password}") String password){
+            @Value("${spring.datasource.password}") String password) {
         this.jdbcTemplate = jdbcTemplate;
         this.url = url;
         this.username = username;
@@ -31,7 +35,33 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public List<Customer> findAll() {
-        return null;
+        String sql = "SELECT * FROM customer";
+        List<Customer> customers = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                Customer customer = new Customer(
+                        result.getInt("customer_id"),
+                        result.getString("first_name"),
+                        result.getString("last_name"),
+                        result.getString("company"),
+                        result.getString("address"),
+                        result.getString("city"),
+                        result.getString("state"),
+                        result.getString("country"),
+                        result.getString("postal_code"),
+                        result.getString("phone"),
+                        result.getString("fax"),
+                        result.getString("email")
+                );
+                customers.add(customer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return customers;
     }
 
     @Override
