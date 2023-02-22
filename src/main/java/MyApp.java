ae1c8489eac64a06c20450fe5d31f3a7c6f4b26f
mosh98 <mosleh.edu@gmail.com>
2023-02-22 13:13:45 +0100

    commit to master with all the changes from ida

--- src/main/java/MyApp.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class MyApp {
-    public static void main(String[] args) {
-     SpringApplication.run(MyApp.class, args);
-    }
-}
